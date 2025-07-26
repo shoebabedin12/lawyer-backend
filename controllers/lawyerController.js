18b5ea41583ddc4f155890b3d7811c00691a5fe3
shoebabedin <shoebabedin12@gmail.com>
2025-07-26 20:41:22 +0600

    jkg

--- controllers/lawyerController.js
+++ controllers/lawyerController.js
@@ -1,17 +1,25 @@
-const { Lawyer, User } = require('../models');
+const { Lawyer, User } = require("../models");
 
 const getAll = async (req, res) => {
   try {
-    const lawyers = await Lawyer.findAll({
+    const lawyers = await Lawyer.findAndCountAll({
       include: [
         {
           model: User,
-          as: 'User',
-          attributes: ['id', 'name', 'email']
-        }
-      ]
+          as: "User",
+          attributes: ["id", "name", "email"],
+        },
+      ],
+      limit: 10,
+      offset: 0,
+      order: [["createdAt", "DESC"]],
     });
-    res.json(lawyers);
+    // res.json(lawyers);
+    // Structure response
+res.status(200).json({
+  total: lawyers.count,     // 🔢 total number of lawyers
+  lawyers: lawyers.rows,    // 🧾 array of lawyer objects
+});
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -20,7 +28,7 @@ const getAll = async (req, res) => {
 const getOne = async (req, res) => {
   try {
     const lawyer = await lawyerModel.getLawyerById(req.params.id);
-    if (!lawyer) return res.status(404).json({ message: 'Not found' });
+    if (!lawyer) return res.status(404).json({ message: "Not found" });
     res.json(lawyer);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -48,10 +56,10 @@ const update = async (req, res) => {
 const remove = async (req, res) => {
   try {
     await lawyerModel.deleteLawyer(req.params.id);
-    res.json({ message: 'Deleted successfully' });
+    res.json({ message: "Deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-module.exports = { getAll, getOne, create, update, remove};
+module.exports = { getAll, getOne, create, update, remove };
